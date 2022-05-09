3646518320e73a3f89ebe6594347ccedda957ef9
Mond <herecomesthemoon@protonmail.com>
2022-05-09 17:40:05 +0200

    Autogenerated commit for submission 696187400

--- add-binary/solution.py
+++ add-binary/solution.py
@@ -1,3 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return str(bin(int(a, 2) + int(b, 2)))[2:]
+        if len(a) < len(b):
+            b, a = a, b
+        carry = False
+        result = [None] * len(a)
+
+        for k, (x, y) in enumerate(zip(reversed(a), reversed("0" * (len(a) - len(b)) + b))):
+            if x == y:
+                result[k] = carry
+                carry = x == "1"
+            else:
+                result[k] = not carry
+
+        result = "".join(("1" if x else "0" for x in reversed(result)))
+        if carry:
+            return "1" + result
+        return result
