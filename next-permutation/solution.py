8ef81749b0903a38b0c750ffdbb3bbb200cc7853
Mond <herecomesthemoon@protonmail.com>
2022-05-10 13:59:51 +0200

    Autogenerated commit for submission 696742978

--- next-permutation/solution.py
+++ next-permutation/solution.py
@@ -1,18 +1,39 @@
+
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
+    def nextPermutation(self, nums: list[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for k in range(len(nums) - 2, -1, -1):
-            if nums[k] < nums[k + 1]: 
-                i = len(nums) - 1
-                while nums[k] >= nums[i]:
-                    i -= 1
+        k = len(nums) - 2
+        while k >= 0:
+            if nums[k] < nums[k + 1]:
+                i = k + 1 + self.binSearch(nums[k+1:], nums[k])
                 nums[k], nums[i] = nums[i], nums[k]
-
-                for i in range(1, (len(nums) + 1 - k) // 2):
-                    nums[k + i], nums[-i] = nums[-i], nums[k + i]
+                nums[k+1:] = reversed(nums[k+1:])
                 return None
-            
+            k -= 1
+
         nums.reverse()
         return None
+
+    def binSearch(self, nums: list[int], goal: int) -> int:
+        """Search in descending sorted list nums[l:r] until you find element > goal. Return index ."""
+        if len(nums) == 1 or nums[-1] > goal:
+            return len(nums) - 1
+
+        j = len(nums) // 2
+        k = j
+
+        while k >= 1:
+            k = k // 2
+            if nums[j] <= goal:
+                j -= k
+            elif nums[j+1] >= goal:
+                j += k
+
+        while nums[j] <= goal:
+            j -= 1
+        while nums[j+1] > goal:
+            j += 1
+
+        return j
