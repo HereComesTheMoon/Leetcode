e10dd8b6c18d20b41f19978a2eadc7ae6a3fe85a
Mond <herecomesthemoon@protonmail.com>
2022-05-12 19:57:17 +0200

    Autogenerated commit for submission 698225238

--- redistribute-characters-to-make-all-strings-equal/solution.py
+++ redistribute-characters-to-make-all-strings-equal/solution.py
@@ -1,3 +1,3 @@
 class Solution:
     def makeEqual(self, words: List[str]) -> bool:
-        return not any(x % len(words) for x in Counter("".join(words)).values())
+        return not any(x % len(words) for x in Counter(letter for word in words for letter in word).values())
