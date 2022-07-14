9c88408acf0410e64e57795e11f4b8c3c50654e5
Mond <herecomesthemoon@protonmail.com>
2022-07-14 11:26:08 +0200

    Autogenerated commit for submission 746816472

--- roman-to-integer/solution.go
+++ roman-to-integer/solution.go
@@ -1,12 +1,12 @@
 func romanToInt(s string) int {
-    conv := map[byte]int {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000,
+    conv := map[string]int {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
     }
 
     conv2 := map[string]int {
@@ -25,11 +25,11 @@ func romanToInt(s string) int {
             num += val
             k++
         } else {
-            num += conv[s[k-1]]
+            num += conv[s[k-1:k]]
         }
     }
     if k == len(s) {
-        num += conv[s[len(s)-1]]
+        num += conv[s[len(s)-1:]]
     }
     return num
 }
