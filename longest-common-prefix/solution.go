d5e574212a920b9370fded8e62dcddbec4aa3642
Mond <herecomesthemoon@protonmail.com>
2022-07-14 12:11:44 +0200

    Autogenerated commit for submission 746841028

--- longest-common-prefix/solution.go
+++ longest-common-prefix/solution.go
@@ -1,11 +1,14 @@
 func longestCommonPrefix(strs []string) string {
     prefix := strs[0]
-    for k := 0; k < len(prefix); k++ {
-        for _, s := range(strs) {
-            if len(s) <= k || s[k] != prefix[k] {
-                return prefix[:k]
+
+    for k := 1; k < len(strs); k++ {
+        for i := 0; i < len(prefix); i++ {
+            if len(strs[k]) <= i || strs[k][i] != prefix[i] {
+                prefix = prefix[:i]
+                break
             }
         }
     }
+
     return prefix
 }
