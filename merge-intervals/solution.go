5a68287a4ab010e100b0ae9ce30580e6d6979c1e
Mond <herecomesthemoon@protonmail.com>
2022-08-06 12:41:44 +0200

    Autogenerated commit for submission 766565814

--- merge-intervals/solution.go
+++ merge-intervals/solution.go
@@ -1,26 +1,26 @@
 func merge(v [][]int) [][]int {
     sort.Slice(v, func(i,j int) bool { return v[i][0] < v[j][0] })
 
-    lower := v[0][0]
-    upper := v[0][1]
-    
     results := make([][]int, 0, len(v))
+    p := v[0]
 
     for _, iv := range v[1:] {
-        if iv[0] <= upper {
-            if upper < iv[1] {
-                upper = iv[1]
+        if iv[0] <= p[1] {
+            if p[1] < iv[1] {
+                p[1] = iv[1]
             }
         } else {
-            results = append(results, []int{lower, upper})
-            lower = iv[0]
-            upper = iv[1]
+            results = append(results, p)
+            p = iv
         }
     }
-    results = append(results, []int{lower, upper})
+    results = append(results, p)
 
     return results
 }
+
+
+
 
 
 
