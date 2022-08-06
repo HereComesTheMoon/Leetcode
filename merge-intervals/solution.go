3dc464f6269c6bff73fe408e266cfc1d6d7fcff4
Mond <herecomesthemoon@protonmail.com>
2022-08-06 12:25:34 +0200

    Autogenerated commit for submission 766556820

--- merge-intervals/solution.go
+++ merge-intervals/solution.go
@@ -1,28 +1,12 @@
-type ivs [][]int
-
-func (v ivs) Len() int {
-    return len(v)
-}
-
-func (v ivs) Swap(i, j int) {
-    v[i], v[j] = v[j], v[i]
-}
-
-func (v ivs) Less(i, j int) bool {
-    return v[i][0] < v[j][0]
-}
-
-
 func merge(v [][]int) [][]int {
-    var w ivs = v
-    sort.Sort(w)
+    sort.Slice(v, func(i,j int) bool { return v[i][0] < v[j][0] })
 
-    lower := w[0][0]
-    upper := w[0][1]
+    lower := v[0][0]
+    upper := v[0][1]
     
-    results := make([][]int, 0, len(w))
+    results := make([][]int, 0, len(v))
 
-    for _, iv := range w[1:] {
+    for _, iv := range v[1:] {
         if iv[0] <= upper {
             if upper < iv[1] {
                 upper = iv[1]
@@ -37,4 +21,6 @@ func merge(v [][]int) [][]int {
 
     return results
 }
+
+
 
