f48f4f49ff5bb93210b72ba1a8e1967dbb808fbd
Mond <herecomesthemoon@protonmail.com>
2022-08-12 19:02:17 +0200

    Autogenerated commit for submission 772028842

--- find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
+++ find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int strStr(std::string haystack, std::string needle) {
-        for(size_t i = 0; i < haystack.length(); ++i) {
-            if (needle == haystack.substr(i, needle.length())) {
-                return i;
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        const size_t upper = haystack.length() - needle.length() + 1;
+        for(size_t i = 0; i < upper; ++i) {
+            for (size_t j = 0; j < needle.length(); ++j) {
+                if (haystack[i+j] != needle[j]) {
+                    goto BREAK_NESTED;
+                }
             }
+            return i;
+BREAK_NESTED:
+            ;
         }
         return -1;
     }
