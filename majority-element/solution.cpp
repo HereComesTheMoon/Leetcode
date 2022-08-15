404b340f7f67ba06baa34c7568c1e852d07e5966
Mond <herecomesthemoon@protonmail.com>
2022-08-15 11:29:31 +0200

    Autogenerated commit for submission 774154050

--- majority-element/solution.cpp
+++ majority-element/solution.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int majorityElement(std::vector<int>& nums) {
-        if( nums.size() == 1 ) {
-            return nums[0];
-        }
-        std::map<int, size_t> count = {};
-        for( auto x : nums ) {
-            if ( count.find(x) != count.end() ) {
-                count[x]++;
-                if ( count[x] > nums.size()/2 ) {
-                    return x;
-                }
-            } else {
-                count[x] = 1;
+        std::mt19937 rng = std::mt19937{std::random_device{}()};
+        std::uniform_int_distribution<std::mt19937::result_type> sample(0, nums.size() - 1);
+        while ( true ) {
+            int last = nums[sample(rng)];
+            size_t count = 0;
+            for ( auto e : nums ) {
+                count += (e == last);
+            }
+            if ( count > nums.size()/2 ) {
+                return last;
             }
         }
-        __builtin_unreachable();
     }
 };
