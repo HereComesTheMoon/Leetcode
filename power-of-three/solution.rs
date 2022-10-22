b8e04ff61b98771a846f685be9a4e9387b216933
Mond <herecomesthemoon@protonmail.com>
2022-10-22 17:08:46 +0200

    Autogenerated commit for submission 827969603

--- power-of-three/solution.rs
+++ power-of-three/solution.rs
@@ -1,6 +1,8 @@
 impl Solution {
     pub fn is_power_of_three(n: i32) -> bool {
-        let res = (n as f64).log(3.0);
-        return (res - (res.round() as f64)).abs() < 0.0000000000001;
+        match n {
+            1 | 3 | 9 | 27 | 81 | 243 | 729 | 2187 | 6561 | 19683 | 59049 | 177147 | 531441 | 1594323 | 4782969 | 14348907 | 43046721 | 129140163 | 387420489 | 1162261467 => true,
+            _ => false,
+        }
     }
 }
