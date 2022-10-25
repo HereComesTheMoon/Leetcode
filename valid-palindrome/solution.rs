38ab43ac55a2a376142c8117993f57b4a65bc512
Mond <herecomesthemoon@protonmail.com>
2022-10-25 11:44:54 +0200

    Autogenerated commit for submission 829856778

--- valid-palindrome/solution.rs
+++ valid-palindrome/solution.rs
@@ -1,11 +1,20 @@
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        let mut a = s.chars().rev().filter(|c| c.is_alphanumeric()).map(|c| c.to_ascii_lowercase());
-        for x in s.chars().filter(|c| c.is_alphanumeric()).map(|c| c.to_ascii_lowercase()) {
+        let mut a = s
+            .as_bytes()
+            .into_iter()
+            .rev()
+            .filter(|c| c
+                    .is_ascii_alphanumeric())
+            .map(|c| c
+                 .to_ascii_lowercase());
+
+        for x in s.as_bytes().into_iter().filter(|c| c.is_ascii_alphanumeric()).map(|c| c.to_ascii_lowercase()) {
             if x != a.next().unwrap() {
                 return false
             }
         }
+
         true
     }
 }
