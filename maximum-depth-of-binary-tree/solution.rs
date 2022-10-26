e8218269ceff83623c6cb57d1c787871091a6f7a
Mond <herecomesthemoon@protonmail.com>
2022-10-26 18:13:11 +0200

    Autogenerated commit for submission 830809996

---
+++ maximum-depth-of-binary-tree/solution.rs
@@ -0,0 +1,36 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        max_depth_helper(&root)
+    }
+}
+
+pub fn max_depth_helper(root: &Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    if let Some(root) = &root {
+        return 1 + i32::max(
+            max_depth_helper(&root.as_ref().borrow().left),
+            max_depth_helper(&root.as_ref().borrow().right)
+            )
+    }
+    0
+}
