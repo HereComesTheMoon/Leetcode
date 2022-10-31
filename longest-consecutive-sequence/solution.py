c4ba0cd0586a4292c97a37230ddd8db644405995
Mond <herecomesthemoon@protonmail.com>
2022-10-31 14:44:56 +0100

    Autogenerated commit for submission 834018262

---
+++ longest-consecutive-sequence/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        vals = { x for x in nums }
+        result = 0
+        while vals:
+            x = vals.pop()
+            vals.add(x)
+
+            while x-1 in vals:
+                x = x - 1
+            
+            current = 0
+            while x in vals:
+                vals.discard(x)
+                current += 1
+                x = x + 1
+                
+            result = max(result, current)
+        return result
+            
+            
+        
