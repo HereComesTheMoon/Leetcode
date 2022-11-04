a759d07618f0aa54901bd162b8cef8ed36ba4679
Mond <herecomesthemoon@protonmail.com>
2022-11-04 18:57:35 +0100

    Autogenerated commit for submission 836898414

--- jump-game/solution.rs
+++ jump-game/solution.rs
@@ -1,14 +1,13 @@
 impl Solution {
     pub fn can_jump(nums: Vec<i32>) -> bool {
-        let mut need_to_reach = nums.len() - 1;
         
-        let mut i = nums.len() - 1;
-        while i != 0 {
-            if i + nums[i] as usize >= need_to_reach {
-                need_to_reach = i;
-            }
-            i -= 1;
-        }
-        need_to_reach <= nums[0] as usize
+        (0..nums.len()-1)
+            .rev()
+            .fold(nums.len() - 1, |need_to_reach, i|
+                if i + nums[i] as usize >= need_to_reach {
+                    i
+                } else {
+                    need_to_reach
+                }) <= nums[0] as usize
     }
 }
