39901515e99b92be14d3513d7c41c0abb5852b2f
Mond <herecomesthemoon@protonmail.com>
2022-11-04 11:41:59 +0100

    Autogenerated commit for submission 836682476

---
+++ jump-game/solution.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn can_jump(nums: Vec<i32>) -> bool {
+        let mut need_to_reach = nums.len() - 1;
+        
+        let mut i = nums.len() - 1;
+        while i != 0 {
+            if i + nums[i] as usize >= need_to_reach {
+                need_to_reach = i;
+            }
+            i -= 1;
+        }
+        need_to_reach <= nums[0] as usize
+    }
+}
