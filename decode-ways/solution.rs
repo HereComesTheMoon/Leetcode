896a02af8aed884aead264bd3cfe078272794b59
Mond <herecomesthemoon@protonmail.com>
2022-11-10 21:29:08 +0100

    Autogenerated commit for submission 840965227

--- decode-ways/solution.rs
+++ decode-ways/solution.rs
@@ -1,17 +1,20 @@
-
 impl Solution {
     pub fn num_decodings(s: String) -> i32 {
         let s = s.as_bytes();
-        if s[0] == b'0' { return 0 }
+        if s[0] == b'0' {
+            return 0;
+        }
 
         let mut val = 1;
         let mut prev = 1;
         for i in 0..s.len() - 1 {
-            let new = if s[i+1] != b'0' { 1 } else { 0 } * val + prev * match (s[i], s[i+1]) {
-                (b'1', _) => { 1 },
-                (b'2', b'0'..=b'6') => { 1 },
-                _ => { 0 },
-            };
+            let new = if s[i + 1] != b'0' { 1 } else { 0 } * val
+                + prev
+                    * match (s[i], s[i + 1]) {
+                        (b'1', _) => 1,
+                        (b'2', b'0'..=b'6') => 1,
+                        _ => 0,
+                    };
             prev = val;
             val = new;
         }
