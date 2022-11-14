228339d328ba38735efd79e406988293f4da6ab6
Mond <herecomesthemoon@protonmail.com>
2022-11-14 12:33:52 +0100

    Autogenerated commit for submission 843214712

---
+++ valid-parentheses/solution.rs
@@ -0,0 +1,42 @@
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        if s.len() % 2 == 1 {
+            return false;
+        }
+        let s = s.as_bytes();
+        let mut stack = Vec::with_capacity((s.len() + 1) / 2);
+        for c in s {
+            match *c {
+                b'(' => {
+                    stack.push(b'(');
+                }
+                b'[' => {
+                    stack.push(b'[');
+                }
+                b'{' => {
+                    stack.push(b'{');
+                }
+                b')' => {
+                    if let Some(b'(') = stack.pop() {
+                    } else {
+                        return false;
+                    };
+                }
+                b']' => {
+                    if let Some(b'[') = stack.pop() {
+                    } else {
+                        return false;
+                    };
+                }
+                b'}' => {
+                    if let Some(b'{') = stack.pop() {
+                    } else {
+                        return false;
+                    };
+                }
+                _ => panic!(),
+            }
+        }
+        stack.is_empty()
+    }
+}
