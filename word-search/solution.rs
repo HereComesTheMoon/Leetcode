c35f38066061d4da9b39dcd1106fef68e66c3af1
Mond <herecomesthemoon@protonmail.com>
2022-11-24 12:36:53 +0100

    Autogenerated commit for submission 849089182

---
+++ word-search/solution.rs
@@ -0,0 +1,42 @@
+
+impl Solution {
+    pub fn exist(board: Vec<Vec<char>>, word: String) -> bool {
+        let board: Vec<Vec<u8>> = board.into_iter().map(|row| row.iter().map(|c| *c as u8).collect::<Vec<u8>>()).collect();
+        let mut visited = (0..board.len()).map(|_| vec![false; board[0].len()]).collect();
+        let word = word.as_bytes();
+        for y in 0..board.len() {
+            for x in 0..board[0].len() {
+                if rec(&board, &mut visited, word, (x as isize, y as isize)) {
+                    return true
+                }
+            }
+        }
+        false
+    }
+}
+
+
+pub fn rec(board: &Vec<Vec<u8>>, visited: &mut Vec<Vec<bool>>, word: &[u8], pos: (isize, isize)) -> bool {
+    let test = visited.get(pos.1 as usize).and_then(|row| row.get(pos.0 as usize));
+    if test.is_none() || *test.unwrap() { return false; }
+    
+    if board[pos.1 as usize][pos.0 as usize] != word[0] { return false; }
+    
+    if word.len() == 1 { return true; }
+    
+    visited[pos.1 as usize][pos.0 as usize] = true;
+    if rec(board, visited, &word[1..], (pos.0 - 1, pos.1)) {
+        return true;
+    }
+    if rec(board, visited, &word[1..], (pos.0 + 1, pos.1)) {
+        return true;
+    }
+    if rec(board, visited, &word[1..], (pos.0, pos.1 - 1)) {
+        return true;
+    }
+    if rec(board, visited, &word[1..], (pos.0, pos.1 + 1)) {
+        return true;
+    }
+    visited[pos.1 as usize][pos.0 as usize] = false;
+    return false;
+}
