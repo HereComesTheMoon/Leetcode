05911fd1de961f92df1cffcebfc4fa2a99f0e75e
Mond <herecomesthemoon@protonmail.com>
2023-01-19 10:57:53 +0100

    Autogenerated commit for submission 881119793

--- two-sum-ii-input-array-is-sorted/solution.py
+++ two-sum-ii-input-array-is-sorted/solution.py
@@ -1,12 +1,13 @@
+from bisect import bisect_right, bisect_left
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         i = 0
         j = len(nums) - 1
         while True:
             if nums[i] + nums[j] < target:
-                i += 1
+                i = bisect_left(nums, target - nums[j], i, j)
             elif nums[i] + nums[j] > target:
-                j -= 1
+                j = bisect_right(nums, target - nums[i], i, j) - 1
             else:
                 break
         return [i + 1, j + 1]
