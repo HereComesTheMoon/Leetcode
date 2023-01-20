75da69c41f06706a89e623ccc5a4c8ad6326c0c7
Mond <herecomesthemoon@protonmail.com>
2023-01-20 19:07:15 +0100

    Autogenerated commit for submission 881972130

--- two-sum/solution.py
+++ two-sum/solution.py
@@ -1,8 +1,7 @@
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
-        for k, v in enumerate(nums):
-            b = d.get(target - v, None)
-            if b is not None:
-                return [k, b]
-            d[v] = k
+        for k, x in enumerate(nums):
+            if x in d:
+                return [d[x], k]
+            d[target - x] = k
