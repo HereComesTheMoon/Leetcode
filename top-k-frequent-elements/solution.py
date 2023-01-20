04866a32e1ea8fa0c1a4a1cd5513d85c40300317
Mond <herecomesthemoon@protonmail.com>
2023-01-20 19:28:10 +0100

    Autogenerated commit for submission 881984488

--- top-k-frequent-elements/solution.py
+++ top-k-frequent-elements/solution.py
@@ -2,5 +2,4 @@ from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return ( v for (v, _) in Counter(nums).most_common(k) )
-        
+        return [v for v, _ in Counter(nums).most_common(k)]
