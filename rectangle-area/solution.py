5476e923d14c2a62c949ecb2ec926d3ac55bfd4d
Mond <herecomesthemoon@protonmail.com>
2023-01-20 10:10:16 +0100

    Autogenerated commit for submission 881727568

---
+++ rectangle-area/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        areaa = (ax2 - ax1) * (ay2 - ay1)
+        areab = (bx2 - bx1) * (by2 - by1)
+        cx1 = max(ax1, bx1)
+        cx2 = min(ax2, bx2)
+        cy1 = max(ay1, by1)
+        cy2 = min(ay2, by2)
+        return areaa + areab - max(cx2 - cx1, 0) * max(cy2 - cy1, 0)
