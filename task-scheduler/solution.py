b4e8e44f2047802520fdf392d030ab08fb05e3c6
Mond <herecomesthemoon@protonmail.com>
2023-01-22 13:57:26 +0100

    Autogenerated commit for submission 883067806

--- task-scheduler/solution.py
+++ task-scheduler/solution.py
@@ -5,18 +5,17 @@ class Solution:
         d = {}
         for x in tasks:
             d[x] = d.get(x, 0) + 1
-        heap = [[0, -count] for count in d.values()]
+        heap = [(0, -count) for count in d.values()]
         pq.heapify(heap)
         time = -1
         while heap:
             time += 1
-            for x in heap:
-                x[0] = max(x[0], time)
             if time < heap[0][0]:
                 continue
-            pq.heapify(heap)
+            while heap[0][0] < time:
+                pq.heapreplace(heap, (time, heap[0][1]))
             if -heap[0][1] == 1:
                 pq.heappop(heap)
                 continue
-            pq.heapreplace(heap, [time + n + 1, heap[0][1] + 1])
+            pq.heapreplace(heap, (time + n + 1, heap[0][1] + 1))
         return time + 1
