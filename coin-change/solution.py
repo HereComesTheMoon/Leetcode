84d58b9c56c2d80baaebbc152ddc180a0fb189c8
Mond <herecomesthemoon@protonmail.com>
2023-01-23 16:01:06 +0100

    Autogenerated commit for submission 883756150

--- coin-change/solution.py
+++ coin-change/solution.py
@@ -1,17 +1,23 @@
+import heapq as pq
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        needed = [-1 for _ in range(amount + 1)]
-        needed[0] = 0
-        for k in range(amount + 1):
+        if amount == 0:
+            return 0
+        if amount in coins:
+            return 1
+        heap = [ (1, coin) for coin in coins ]
+        seen = set(coins)
+        while heap:
+            num, val = pq.heappop(heap)
             for coin in coins:
-                if k < coin:
+                if val + coin == amount:
+                    return num + 1
+                if amount < val + coin:
                     continue
-                if needed[k - coin] == -1:
+                if val + coin in seen:
                     continue
-                if needed[k] == -1:
-                    needed[k] = needed[k - coin] + 1
-                needed[k] = min(needed[k], needed[k - coin] + 1)
-        return needed[amount]
-
-
-
+                seen.add(val + coin)
+                pq.heappush(heap, (num + 1, val + coin))
+        return -1
+                
