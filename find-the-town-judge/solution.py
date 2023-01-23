33692b70cfd835054f9ced6d3a3c448efbd1da28
Mond <herecomesthemoon@protonmail.com>
2023-01-23 13:38:55 +0100

    Autogenerated commit for submission 883677375

--- find-the-town-judge/solution.py
+++ find-the-town-judge/solution.py
@@ -2,10 +2,10 @@ class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         if n == 1:
             return 1
-        d_in: dict[int, int] = {}
+        d_in = defaultdict(int)
         d_out: set[int] = set()
         for edge in trust:
-            d_in[edge[1]] = d_in.get(edge[1], 0) + 1
+            d_in[edge[1]] += 1
             d_out.add(edge[0])
         for k, v in d_in.items():
             if v < n - 1:
