687f9c61cb4ed34878b58adca68aea14e7257519
Mond <herecomesthemoon@protonmail.com>
2023-01-23 14:26:23 +0100

    Autogenerated commit for submission 883701233

--- triangle/solution.py
+++ triangle/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        cost = [
-            [0 for _ in row] for row in triangle
-        ]
-        cost[0][0] = triangle[0][0]
+        cost = [0 for _ in triangle[-1]]
+        cost[0] = triangle[0][0]
         for y in range(1, len(triangle)):
-            cost[y][0] = cost[y-1][0] + triangle[y][0]
+            temp = cost[0]
+            cost[0] = temp + triangle[y][0]
             for x in range(1, len(triangle[y]) - 1):
-                cost[y][x] = triangle[y][x] + min(cost[y-1][x-1], cost[y-1][x])
-            cost[y][-1] = cost[y-1][-1] + triangle[y][-1]
-        return min(cost[-1])
+                new_val = triangle[y][x] + min(temp, cost[x])
+                temp = cost[x]
+                cost[x] = new_val
+            cost[len(triangle[y]) - 1] = temp + triangle[y][-1]
+        return min(cost)
