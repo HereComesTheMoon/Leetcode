e14b8d651f8626acc57134883847c58f7b230e12
Mond <herecomesthemoon@protonmail.com>
2023-01-26 11:35:34 +0100

    Autogenerated commit for submission 885592396

--- cheapest-flights-within-k-stops/solution.py
+++ cheapest-flights-within-k-stops/solution.py
@@ -1,44 +1,18 @@
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        d = { x : [] for x in range(n) }
-        shortest = -1
-        found = False
-        for start, goal, price in flights:
-            shortest = max(shortest, price)
-            d[start].append((goal, price))
-        
-        new_reachable = { src }
-        reachable = { src }
-        while new_reachable:
-            now = new_reachable.pop()
-            for goal, _ in d[now]:
-                if goal not in reachable:
-                    new_reachable.add(goal)
-                    reachable.add(goal)
-        if dst not in reachable:
-            return -1
-        
-
-
-        shortest = shortest * (k + 2)
-
-        stack = [
-            (0, src, -1)
-        ]
-
-        while stack:
-            price, pos, num_stops = stack.pop()
-            if pos == dst:
-                shortest = min(shortest, price)
-                found = True
-            if num_stops == k:
-                continue
-            for goal, cost in d[pos]:
-                if shortest < price + cost:
-                    continue
-                stack.append((price + cost, goal, num_stops + 1))
-        
-        if found:
-            return shortest
-        else:
-            return -1
+    def findCheapestPrice(self, n, flights, src, dst, k):
+        adj = defaultdict(list)
+        for s, d, p in flights:
+            adj[s].append((d, p))
+        queue = deque()
+        queue.append((src, 0))
+        costs = [float("inf")] * n
+        while queue and k >= 0:
+            m = len(queue)
+            for _ in range(m):
+                curr, cost = queue.popleft()
+                for node, price in adj[curr]:
+                    if cost + price < costs[node]:
+                        costs[node] = cost + price
+                        queue.append((node, costs[node]))
+            k -= 1
+        return costs[dst] if costs[dst] != float("inf") else -1
