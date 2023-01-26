e1c1b78b0bed24346cefb7d0564a2924bef70305
Mond <herecomesthemoon@protonmail.com>
2023-01-26 11:48:20 +0100

    Autogenerated commit for submission 885597838

--- cheapest-flights-within-k-stops/solution.py
+++ cheapest-flights-within-k-stops/solution.py
@@ -1,18 +1,24 @@
+
 class Solution:
-    def findCheapestPrice(self, n, flights, src, dst, k):
-        adj = defaultdict(list)
-        for s, d, p in flights:
-            adj[s].append((d, p))
-        queue = deque()
-        queue.append((src, 0))
-        costs = [float("inf")] * n
-        while queue and k >= 0:
-            m = len(queue)
-            for _ in range(m):
-                curr, cost = queue.popleft()
-                for node, price in adj[curr]:
-                    if cost + price < costs[node]:
-                        costs[node] = cost + price
-                        queue.append((node, costs[node]))
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        d = { x : [] for x in range(n) }
+        for start, goal, price in flights:
+            d[start].append((goal, price))
+        dists = [float("inf") for _ in range(n)]
+        q = deque([
+            (src, 0)
+        ])
+
+        while q and 0 <= k:
+            for i in range(len(q)):
+                pos, cost = q.popleft()
+                for goal, price in d[pos]:
+                    if dists[goal] <= cost + price:
+                        continue
+                    dists[goal] = cost + price
+                    q.append((goal, cost + price))
             k -= 1
-        return costs[dst] if costs[dst] != float("inf") else -1
+
+        if dists[dst] == float("inf"):
+            return -1
+        return dists[dst]
