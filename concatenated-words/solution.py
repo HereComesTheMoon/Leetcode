10b1c75de866eb5a3b2c9f1f7e6e1b09ed3c7650
Mond <herecomesthemoon@protonmail.com>
2023-01-27 14:57:01 +0100

    Autogenerated commit for submission 886293427

--- concatenated-words/solution.py
+++ concatenated-words/solution.py
@@ -1,23 +1,15 @@
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
         seen = set(words)
-        inconstructible = set()
         constructed = []
 
         def rec(word: str):
             if word in seen:
                 return True
-            if word in inconstructible:
-                return False
             for i in range(1, len(word)):
-                if word[:i] in seen:
-                    check = rec(word[i:])
-                    if check:
-                        seen.add(word)
-                        return True
-                    else:
-                        inconstructible.add(word)
-            return False
+                if word[:i] in seen and rec(word[i:]):
+                    seen.add(word)
+                    return True
 
         for word in words:
             for i in range(1, len(word)):
