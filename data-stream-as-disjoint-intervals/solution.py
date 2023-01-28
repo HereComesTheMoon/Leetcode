16343872536b300fc00b3b931f563c5e73ecc46b
Mond <herecomesthemoon@protonmail.com>
2023-01-28 12:46:45 +0100

    Autogenerated commit for submission 886802331

--- data-stream-as-disjoint-intervals/solution.py
+++ data-stream-as-disjoint-intervals/solution.py
@@ -1,26 +1,29 @@
-from bisect import insort
+from bisect import bisect
 
 class SummaryRanges:
 
     def __init__(self):
-        self.nums = []
+        self.ivs = [[-2, -2], [10_002, 10_002]]
         
 
     def addNum(self, value: int) -> None:
-        insort(self.nums, value)
-        
+        i = bisect(self.ivs, [value, 10_002])
+        if value <= self.ivs[i-1][1]:
+            return
+        if value == self.ivs[i-1][1] + 1:
+            self.ivs[i-1][1] += 1
+            if self.ivs[i][0] == value + 1:
+                self.ivs[i-1][1] = self.ivs[i][1]
+                self.ivs = self.ivs[:i] + self.ivs[i + 1:]
+            return    
+        if value + 1 == self.ivs[i][0]:
+            self.ivs[i][0] -= 1
+            return
+        self.ivs = self.ivs[:i] + [[value, value]] + self.ivs[i:]
+
 
     def getIntervals(self) -> List[List[int]]:
-        if not self.nums:
-            return []
-        res = [[self.nums[0], self.nums[0]]]
-        for x in self.nums[1:]:
-            if x <= res[-1][1] + 1:
-                res[-1][1] = x
-            else:
-                res.append([x, x])
-        return res
-        
+        return self.ivs[1:len(self.ivs)-1]
 
 
 # Your SummaryRanges object will be instantiated and called as such:
