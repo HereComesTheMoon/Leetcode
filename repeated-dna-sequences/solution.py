20d75f8b54fe43bcc2ae874819316f342b95b286
Mond <herecomesthemoon@protonmail.com>
2023-02-13 11:13:49 +0100

    Autogenerated commit for submission 897099029

---
+++ repeated-dna-sequences/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        if len(s) <= 10:
+            return []
+        counter = defaultdict(int)
+        for i in range(len(s) + 1 - 10):
+            counter[s[i:i+10]] += 1
+        
+        res = []
+        print(counter)
+        for k, v in counter.items():
+            if v <= 1:
+                continue
+            res.append(k)
+        return res
+        
