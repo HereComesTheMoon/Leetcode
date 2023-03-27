cf79d05c16c20cc7b672d0dfa95d0b59653da699
Mond <herecomesthemoon@protonmail.com>
2023-03-27 12:04:40 +0200

    Autogenerated commit for submission 922935038

--- minimum-path-sum/solution.py
+++ minimum-path-sum/solution.py
@@ -2,15 +2,20 @@ import heapq as pq
 
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        heap: List[(int, (int, int))] = [(grid[0][0], (0, 0))]
+        heap = [
+            (grid[0][0], 0, 0)
+        ]
         seen = set()
-        while True:
-            weight, (x, y) = pq.heappop(heap)
+        while heap:
+            weight, x, y = pq.heappop(heap)
+            if (x, y) in seen:
+                continue
             if x == len(grid[0]) - 1 and y == len(grid) - 1:
                 return weight
-            if x + 1 < len(grid[0]) and (x+1, y) not in seen:
-                pq.heappush(heap, (weight + grid[y][x+1], (x + 1, y)))
-                seen.add((x+1, y))
-            if y + 1 < len(grid) and (x, y+1) not in seen:
-                pq.heappush(heap, (weight + grid[y+1][x], (x, y + 1)))
-                seen.add((x, y+1))
+            seen.add((x, y))
+
+            if y < len(grid) - 1:
+                pq.heappush(heap, (weight + grid[y + 1][x], x, y + 1))
+            if x < len(grid[0]) - 1:
+                pq.heappush(heap, (weight + grid[y][x + 1], x + 1, y))
+        return -1
