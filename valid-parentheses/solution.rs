621d1bb0df4184cf1e5e007c49b41441bb8412a6
Mond <herecomesthemoon@protonmail.com>
2023-04-10 19:19:00 +0200

    Autogenerated commit for submission 931413593

--- valid-parentheses/solution.rs
+++ valid-parentheses/solution.rs
@@ -1,42 +1,30 @@
 impl Solution {
     pub fn is_valid(s: String) -> bool {
-        if s.len() % 2 == 1 {
-            return false;
-        }
-        let s = s.as_bytes();
-        let mut stack = Vec::with_capacity((s.len() + 1) / 2);
-        for c in s {
-            match *c {
-                b'(' => {
-                    stack.push(b'(');
-                }
-                b'[' => {
-                    stack.push(b'[');
-                }
-                b'{' => {
-                    stack.push(b'{');
-                }
-                b')' => {
-                    if let Some(b'(') = stack.pop() {
+        let mut stack = vec![];
+        for c in s.chars() {
+            match c {
+                '(' | '{' | '[' => stack.push(c),
+                ')' => {
+                    if let Some('(') = stack.pop() {
                     } else {
                         return false;
-                    };
-                }
-                b']' => {
-                    if let Some(b'[') = stack.pop() {
+                    }
+                },
+                '}' => {
+                    if let Some('{') = stack.pop() {
                     } else {
                         return false;
-                    };
-                }
-                b'}' => {
-                    if let Some(b'{') = stack.pop() {
+                    }
+                },
+                ']' => {
+                    if let Some('[') = stack.pop() {
                     } else {
                         return false;
-                    };
-                }
-                _ => panic!(),
+                    }
+                },
+                _ => unreachable!(),
             }
         }
-        stack.is_empty()
+        return stack.is_empty();
     }
 }
