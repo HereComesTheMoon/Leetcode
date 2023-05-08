0e33c9aacc9d5d4c6affff3f26f97e8425e52c81
Mond <herecomesthemoon@protonmail.com>
2023-05-08 11:52:16 +0200

    Autogenerated commit for submission 946543418

--- longest-substring-without-repeating-characters/solution.py
+++ longest-substring-without-repeating-characters/solution.py
@@ -3,19 +3,15 @@ class Solution:
         if len(s) <= 1:
             return len(s)
         i = 0
-        seen = { s[0] }
-        res = 1
-        for j in range(1, len(s)):
-            if s[j] not in seen:
-                seen.add(s[j])
-                res = max(res, j + 1 - i)
-                continue
-            while s[i] != s[j]:
+        j = 1
+        seen = { s[i] }
+        res = 0
+        while j < len(s):
+            while s[j] in seen:
                 seen.remove(s[i])
                 i += 1
-            i += 1
-        
+            seen.add(s[j])
+            j += 1
+            res = max(res, j - i)
         return res
-            
 
-            
