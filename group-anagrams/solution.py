17d6aefa63b89f67913a22c310e2e67830f34484
Mond <herecomesthemoon@protonmail.com>
2023-05-10 20:01:33 +0200

    Autogenerated commit for submission 947989340

--- group-anagrams/solution.py
+++ group-anagrams/solution.py
@@ -1,10 +1,7 @@
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d: dict[str, List[str]] = {}
-        for s in strs:
-            key = "".join(sorted(list(s)))
-            if key not in d:
-                d[key] = [s]
-            else:
-                d[key].append(s)
-        return [bucket for bucket in d.values()]
+        d = defaultdict(list)
+        for word in strs:
+            d["".join(sorted(word))].append(word)
+        return d.values()
