5f92bcfeccbd8cbf05a128da4c07f9aa7fb7c783
Mond <herecomesthemoon@protonmail.com>
2023-05-12 11:35:46 +0200

    Autogenerated commit for submission 948931917

--- solving-questions-with-brainpower/solution.py
+++ solving-questions-with-brainpower/solution.py
@@ -1,15 +1,13 @@
-from functools import cache
-
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-
-        @cache
-        def rec(i: int) -> int:
-            if len(questions) <= i:
-                return 0
-            return max(
-                rec(i + 1),
-                questions[i][0] + rec(i + 1 + questions[i][1])
-            )
-
-        return rec(0)
+        n = len(questions)
+        res = [None for _ in questions]
+        res[-1] = questions[-1][0]
+        for k in reversed(range(n - 1)):
+            bp = questions[k][1] + 1
+            if k + bp < n:
+                res[k] = max(res[k+1], questions[k][0] + res[k + bp])
+            else:
+                res[k] = max(res[k+1], questions[k][0])
+        print(res)
+        return res[0]
