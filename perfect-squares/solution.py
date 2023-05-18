e33abdbcb358408dc0d1828d2d381804c14ca275
Mond <herecomesthemoon@protonmail.com>
2023-05-18 17:06:39 +0200

    Autogenerated commit for submission 952783263

--- perfect-squares/solution.py
+++ perfect-squares/solution.py
@@ -3,23 +3,11 @@ from functools import cache
 class Solution:
     def numSquares(self, n: int) -> int:
         squares = [k**2 for k in range(1, isqrt(n) + 1)]
-        squares.reverse()
+        frontier = { 0 }
+        steps = 0
+        while True:
+            steps += 1
+            frontier = { x + square for square in squares for x in frontier }
+            if n in frontier:
+                return steps
 
-        squares_check = set(squares)
-
-        if n in squares_check:
-            return 1
-        @cache
-        def rec(n: int) -> int:
-            if n <= 1:
-                return 1 if n == 1 else 0
-            if n in squares_check:
-                return 1
-            val = float('inf')
-            for x in squares:
-                if n < x:
-                    continue
-                val = min(val, rec(n-x))
-            return val + 1
-        
-        return rec(n)
