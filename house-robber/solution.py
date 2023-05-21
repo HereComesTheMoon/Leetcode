93de48a1a37958ee4d005d8871c7407a37088959
Mond <herecomesthemoon@protonmail.com>
2023-05-21 17:17:42 +0200

    Autogenerated commit for submission 954569757

--- house-robber/solution.py
+++ house-robber/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        A = [nums[0], max(nums[0], nums[1])] + [None]*(len(nums) - 2)
-        for k in range(2, len(nums)):
-            A[k] = max(A[k-1], A[k-2] + nums[k])
-        return A[-1]
-        
-        
+        if len(nums) <= 2:
+            return max(nums)
+        a = nums[0]
+        b = max(nums[0], nums[1])
+        for x in nums[2:]:
+            c = max(a + x, b)
+            a = b
+            b = c
+        return b
